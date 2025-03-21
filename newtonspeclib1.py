9a918fb264537ef59110f7a290bb578b4db2262e
PyMo <moritz.limbacher@gmail.com>
2025-03-21 10:51:09 +0100

    add newtonspeclib1.py to plot8.py GUI to load and process Spectra saved from the newton spectrometer camera

---
+++ newtonspeclib1.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+class newtonspecopener():
+    def __init__(self, root, opennotebook, openframepos):
+        self.root = root
+        self.opennotebook = opennotebook
+        self.openframepos = openframepos
+        self.buildopenframe()
+    
+    def buildopenframe(self):
+        self.openframe = tk.Frame(self.root, bg = "white")
+        
